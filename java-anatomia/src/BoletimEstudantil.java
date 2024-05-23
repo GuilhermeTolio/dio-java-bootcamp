028e27d84a3f298a77b787608d95a16791bb5c99
Guilherme Tolio dos Santos <guilhermetolio@gmail.com>
2024-05-23 14:55:01 -0300

    identacao

---
+++ java-anatomia/src/BoletimEstudantil.java
@@ -0,0 +1,22 @@
+public class BoletimEstudantil {
+public static void main(String[] args) {
+
+//nao identado
+int mediaFinal = 6;
+if(mediaFinal<6)	
+System.out.println("REPROVADO"); 
+else if(mediaFinal==6)
+System.out.println("PROVA MINERVA"); 
+else
+System.out.println("APROVADO"); 	
+
+//identado
+	int mediaFinal = 6;
+	if (mediaFinal < 6)
+		System.out.println("REPROVADO");
+	else if (mediaFinal == 6)
+		System.out.println("PROVA MINERVA");
+	else
+		System.out.println("APROVADO");
+}
+}
