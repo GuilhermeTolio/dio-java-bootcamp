5cef8d99b1c55b839d937a8a726bc37b9fa53e62
Guilherme Tolio dos Santos <guilhermetolio@gmail.com>
2024-05-23 14:51:29 -0300

    Anatomia das classes

--- java-anatomia/src/MinhaClasse.java
+++ java-anatomia/src/MinhaClasse.java
@@ -1,5 +1,53 @@
 public class MinhaClasse {
 public static void main(String[] args) {
+    String BR = "Brasil";
+    double PI = 3.14;
+    int ESTADOS_BRASILEIRO = 27;
+    int ANO_2000 = 2000;
 
+// Declação inválida de variáveis
+
+int numero&um = 1; //Os únicos símbolos permitidos são _ e $
+int 1numero = 1;    //Uma variável não pode começar com númerico
+int numero um = 1; //Não pode ter espaço no nome da variável
+int long = 1; //long faz parte das palavras reservadas da linguagem
+ 
+ // Declaração válida de veriáveis
+int numero$um = 1;
+int numero1 = 1;
+int numeroum = 1;
+int longo = 1;
+		
+// Estrutura
+
+Tipo NomeBemDefinido = Atribuicao (opcional em alguns casos)
+
+// Exemplo
+
+int idade = 23;
+double altura = 1.62;
+Dog spike; //observe que aqui a variável spike não tem valor, é normal
+
+// Estrutura
+
+TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+//Exemplo
+
+int somar (int numeroUm, int numero2)
+
+String formatarCep (long cep)
+
+
+// nao recomendado
+double salMedio = 1500.23  //variável abreviada, o que dificulta a compreensão
+String emails = "aluno@escola.com" //confuso se a variável seria um array ou único e-mail
+String myName = "JOSEPH" //se idioma pt-BR, o valor poder ser de outro idioma mas o nome da variável não 
+
+// recomendado
+double salarioMedio=1500.23;
+String email ="aluno@escola.com";
+String [] emails = {"aluno@escola.com","professor@escola.com"}
+String meuNome = "JOSEPH" 
 }
 }
